d191adfa306ba66cca228ae2f525d4fd5e556353
oguhpereira <oguhpereira@protonmail.com>
2020-12-14 21:11:19 -0300

    [FEAT] Loops

---
+++ loops/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 20
+
+	for number >= 0 {
+		fmt.Println("O número é ", number)
+		number--
+	}
+
+
+	for i:= 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
